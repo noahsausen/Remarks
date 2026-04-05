f378b1cffd6915de71f28c1275d634b777a893b4
noahsausen <noahsausen@gmail.com>
2026-04-05 16:24:25 -0400

    big fix

--- client/src/pages/private/Home.jsx
+++ client/src/pages/private/Home.jsx
@@ -10,50 +10,53 @@ import { formatDistanceToNow } from 'date-fns';
 export default function Home() {
   var decoded;
   const [user, setUser] = useState("");
-  async function verifyToken() {
-    document.getElementById("LoadingBackground").style.display = "flex";
-    try {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        window.location.replace("/");
+
+  useEffect(() => {
+    const generateRelativeDate = (timestamp) => {
+      try {
+        let relativeDate = formatDistanceToNow(timestamp, {addSuffix: true}).replace("about", "");
+        if (relativeDate === "less than a minute ago") {
+          relativeDate = "just now";
+        }
+        return relativeDate;
+      } catch (error) {
+        return "old";
       }
-      const res = await axios.post("https://remarks-server.vercel.app/auth/verify", {token: token});
-      if (res.data.valid) {
-        decoded = jwtDecode(token);
-        setUser(decoded.user);
-      } else {
+    }
+
+    const verifyToken = async () => {
+      document.getElementById("LoadingBackground").style.display = "flex";
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          window.location.replace("/");
+        }
+        const res = await axios.post("https://remarks-server.vercel.app/auth/verify", {token: token});
+        if (res.data.valid) {
+          decoded = jwtDecode(token);
+          setUser(decoded.user);
+        } else {
+          localStorage.removeItem("token");
+          window.location.replace("/");
+          window.location.reload();
+        }
+      } catch (error) {
+        console.log(error);
         localStorage.removeItem("token");
         window.location.replace("/");
         window.location.reload();
       }
-    } catch (error) {
-      console.log(error);
-      localStorage.removeItem("token");
-      window.location.replace("/");
-      window.location.reload();
     }
-  }
 
-  function generateRelativeDate(timestamp) {
-    try {
-      let relativeDate = formatDistanceToNow(timestamp, {addSuffix: true}).replace("about", "");
-      if (relativeDate === "less than a minute ago") {
-        relativeDate = "just now";
-      }
-      return relativeDate;
-    } catch (error) {
-      return "old";
-    }
-  }
-
-  async function updateFeed() {
-    try {
-      const res = await axios.get("https://remarks-server.vercel.app/post/getall");
-      document.getElementById("Feed").innerHTML = "";
-      res.data.posts.forEach((post) => {
-        let postDiv = document.createElement("div");
-        postDiv.className = "Post";
-        postDiv.innerHTML = `
+    const updateFeed = async () => {
+      console.log("Update Feed");
+      try {
+        const res = await axios.get("https://remarks-server.vercel.app/post/getall");
+        document.getElementById("Feed").innerHTML = "";
+        res.data.posts.forEach((post) => {
+          let postDiv = document.createElement("div");
+          postDiv.className = "Post";
+          postDiv.innerHTML = `
           <div>
           <h3><a class="WhiteLinkHidden" href="/user/${post.author}">${post.author}</a></h3>
           <h10>${generateRelativeDate(post.timestamp)}</h10>
@@ -61,18 +64,17 @@ export default function Home() {
           <hr/>
           <p>${post.content}</p>
         `;
-        document.getElementById("Feed").prepend(postDiv);
-      })
-    } catch (error) {
-      console.log(error);
+          document.getElementById("Feed").prepend(postDiv);
+        })
+      } catch (error) {
+        console.log(error);
+      }
+      document.getElementById("LoadingBackground").style.display = "none";
     }
-    document.getElementById("LoadingBackground").style.display = "none";
-  }
 
-  useEffect(() => {
     verifyToken();
     updateFeed();
-  }, [verifyToken, updateFeed]);
+  }, []);
 
 
   return (
--- client/src/pages/private/Profile.jsx
+++ client/src/pages/private/Profile.jsx
@@ -10,71 +10,72 @@ import { formatDistanceToNow } from 'date-fns';
 export default function Profile() {
   const { username } = useParams();
   var decoded;
-  const [/*user,*/ setUser] = useState("");
-  async function verifyToken() {
-    document.getElementById("LoadingBackground").style.display = "flex";
-    try {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        window.location.replace("/");
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const generateRelativeDate = (timestamp) => {
+      try {
+        let relativeDate = formatDistanceToNow(timestamp, {addSuffix: true}).replace("about", "");
+        if (relativeDate === "less than a minute ago") {
+          relativeDate = "just now";
+        }
+        return relativeDate;
+      } catch (error) {
+        return "old";
       }
-      const res = await axios.post("https://remarks-server.vercel.app/auth/verify", {token: token});
-      if (res.data.valid) {
-        decoded = jwtDecode(token);
-        setUser(decoded.user);
-      } else {
+    }
+
+    const verifyToken = async () => {
+      document.getElementById("LoadingBackground").style.display = "flex";
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          window.location.replace("/");
+        }
+        const res = await axios.post("https://remarks-server.vercel.app/auth/verify", {token: token});
+        if (res.data.valid) {
+          decoded = jwtDecode(token);
+          setUser(decoded.user);
+        } else {
+          localStorage.removeItem("token");
+          window.location.replace("/");
+          window.location.reload();
+        }
+      } catch (error) {
+        console.log(error);
         localStorage.removeItem("token");
         window.location.replace("/");
         window.location.reload();
       }
-    } catch (error) {
-      console.log(error);
-      localStorage.removeItem("token");
-      window.location.replace("/");
-      window.location.reload();
     }
-  }
 
-  function generateRelativeDate(timestamp) {
-    try {
-      let relativeDate = formatDistanceToNow(timestamp, {addSuffix: true}).replace("about", "");
-      if (relativeDate === "less than a minute ago") {
-        relativeDate = "just now";
-      }
-      return relativeDate;
-    } catch (error) {
-      return "old";
-    }
-  }
-
-  async function updateFeed() {
-    console.log("Update Feed");
-    try {
-      const res = await axios.post("https://remarks-server.vercel.app/post/getusers", {author: username});
-      document.getElementById("Feed").innerHTML = "";
-      res.data.posts.forEach((post) => {
-        let postDiv = document.createElement("div");
-        postDiv.className = "Post";
-        postDiv.innerHTML = `
+    const updateFeed = async () => {
+      console.log("Update Feed");
+      try {
+        const res = await axios.get("https://remarks-server.vercel.app/post/getall");
+        document.getElementById("Feed").innerHTML = "";
+        res.data.posts.forEach((post) => {
+          let postDiv = document.createElement("div");
+          postDiv.className = "Post";
+          postDiv.innerHTML = `
           <div>
-          <h3>${post.author}</h3>
+          <h3><a class="WhiteLinkHidden" href="/user/${post.author}">${post.author}</a></h3>
           <h10>${generateRelativeDate(post.timestamp)}</h10>
           </div>
           <hr/>
           <p>${post.content}</p>
         `;
-        document.getElementById("Feed").prepend(postDiv);
-      })
-    } catch (error) {
-      console.log(error);
+          document.getElementById("Feed").prepend(postDiv);
+        })
+      } catch (error) {
+        console.log(error);
+      }
+      document.getElementById("LoadingBackground").style.display = "none";
     }
-    document.getElementById("LoadingBackground").style.display = "none";
-  }
 
-  useEffect(() => {
     verifyToken();
     updateFeed();
-  }, [verifyToken, updateFeed]);
+  }, []);
 
 
   return (
